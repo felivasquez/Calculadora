c38639059f879f582b7bdcd489d23a663a4e5b79
Felipe Vasquez <132226848+felivasquez@users.noreply.github.com>
2023-08-03 02:44:48 -0300

    Add files via upload

---
+++ Calculadora.js
@@ -0,0 +1,14 @@
+class Calculadora {
+    suma(num1, num2){
+        return num1 + num2
+    }
+    resta(num1, num2){
+        return num1 - num2
+    }
+    dividir(num1, num2){
+        return num1 / num2
+    }
+    multiplicar(num1, num2){
+        return num1 * num2
+    }
+}
